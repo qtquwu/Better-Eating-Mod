9c8a9a5c372c258d3db4e8c500de53f96ae162c4
qtquwu <66858658+qtquwu@users.noreply.github.com>
2020-07-31 02:29:48 -0600

    Update ClientPreventionMixin.java
    Oops, almost didn't catch a bug!

--- src/main/java/me/qtq/bettereating/mixin/ClientPreventionMixin.java
+++ src/main/java/me/qtq/bettereating/mixin/ClientPreventionMixin.java
@@ -1,13 +1,12 @@
 package me.qtq.bettereating.mixin;
 
 import me.qtq.bettereating.BetterEatingMod;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.UseAction;
@@ -24,16 +23,21 @@ public class ClientPreventionMixin {
     private void preventClientBlockUse(ClientPlayerEntity player, ClientWorld world, Hand hand, BlockHitResult hitresult, CallbackInfoReturnable<ActionResult> Info) {
         if(!BetterEatingMod.foodTimerDone()) {
             ItemStack itemStack = player.getStackInHand(hand);
+
             boolean holdingItem = !player.getMainHandStack().isEmpty() || !player.getOffHandStack().isEmpty();
             boolean itemIsBlock = BlockItem.class.isAssignableFrom(itemStack.getItem().getClass());
 
             boolean useBlockResult = (holdingItem && player.shouldCancelInteraction());
             boolean useItemResult = (!itemStack.isEmpty() && !player.getItemCooldownManager().isCoolingDown(itemStack.getItem()));
 
-            if(BetterEatingMod.blockPlacementRestricted() && useItemResult && itemIsBlock) {
-                Info.setReturnValue(ActionResult.PASS);
+            if(BetterEatingMod.blockUsageRestricted() && useBlockResult) {
+                // See what happens when the block is used, and cancel it if the block ends up being used
+                ActionResult blockUseResult = world.getBlockState(hitresult.getBlockPos()).onUse(world, player, hand, hitresult);
+                if (blockUseResult.isAccepted()) {
+                    Info.setReturnValue(ActionResult.PASS);
+                }
             }
-            if(BetterEatingMod.blockUsageRestricted() && useBlockResult && (!useItemResult || !itemIsBlock)) {
+            if(BetterEatingMod.blockPlacementRestricted() && useItemResult && itemIsBlock) {
                 Info.setReturnValue(ActionResult.PASS);
             }
         }
